d2e47cac1c741a444aed02bfe54d259f2dbfc93e
CarlosDelRosario7 <65183782+CarlosDelRosario7@users.noreply.github.com>
2024-01-21 13:40:37 -0400

    agregando el main.dart (se me había quedado atras)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import './src/app.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+// import 'package:firebase_analytics/firebase_analytics.dart';
+
+import './src/app.dart';
 
 void main() async {
   runApp(const MyApp());
