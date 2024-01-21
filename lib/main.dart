c76b97f1ba655d3fd4eeade0d6e21633feb94f5d
CarlosDelRosario7 <65183782+CarlosDelRosario7@users.noreply.github.com>
2024-01-21 13:20:57 -0400

    intentando que luna lea la imagen del ícono

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import './src/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
 void main() async {
   runApp(const MyApp());
- 
-await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
