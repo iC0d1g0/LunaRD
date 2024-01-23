7743b562813e65cbf6a18ae4be62752ba42c0a0a
adderlis <adderlis@yahoo.com>
2024-01-22 23:02:11 -0400

    signing y register con GOOGLE

--- lib/src/models/authen_firebase.dart
+++ lib/src/models/authen_firebase.dart
@@ -1,14 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+ 
+  //Sigin con EMAIL 
   Future<User?> signInWithEmailAndPassword(
       String email, String password) async {
     try {
       UserCredential result = await _auth.signInWithEmailAndPassword(
         email: email,
-        password: password,
+        password: password, 
       );
       User? user = result.user;
       return user;
@@ -17,7 +19,7 @@ class AuthService {
       return null;
     }
   }
-
+  //Registrar con EMAIL
   Future<User?> registerWithEmailAndPassword(
       String email, String password) async {
     try {
@@ -33,10 +35,81 @@ class AuthService {
     }
 
     // Otros métodos de autenticación, como signUpWithEmailAndPassword, signInWithGoogle, etc., se pueden agregar según sea necesario.
+  
+  }
+  //Signin con GOOGLE
+  Future<User?> signInWithGoogle() async {
+    try {
+      // Inicia el proceso de inicio de sesión con Google
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        // El usuario canceló el inicio de sesión con Google
+        return null;
+      }
+
+      // Obtiene las credenciales de inicio de sesión de Google
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+      // Crea un nuevo objeto de credenciales de acceso de GoogleAuthProvider
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      // Inicia sesión con Firebase usando las credenciales de Google
+      final UserCredential authResult = await _auth.signInWithCredential(credential);
+      final User? user = authResult.user;
+
+      // Verifica si la autenticación fue exitosa
+      if (user != null) {
+        print('Usuario autenticado con éxito: ${user.displayName}');
+      }
+
+      return user;
+    } catch (e) {
+      print('Error al iniciar sesión con Google: $e');//Esto hay que manejarlo diferente. 
+      return null;
+    }
+  }
+ 
+  //Registrar con GOOGLE.
+  Future<User?> registerWithGoogle() async {
+    try {
+     
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+      if (googleSignInAccount == null) {
+        
+        return null;
+      }
+
+      
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+      // Crea un nuevo objeto de credenciales de acceso de GoogleAuthProvider
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      // Registra al usuario con Firebase usando las credenciales de Google
+      final UserCredential authResult = await _auth.signInWithCredential(credential);
+      final User? user = authResult.user;
+
+      // Verifica si el registro fue exitoso
+      if (user != null) {
+        print('Usuario registrado con éxito: ${user.displayName}');
+      }
+
+      return user;
+    } catch (e) {
+      print('Error al registrar con Google: $e');
+      return null;
+     }
   }
 
   // Método para cerrar sesión
   Future<void> signOut() async {
     await _auth.signOut();
+    await googleSignIn.signOut();
   }
 }
--- lib/src/models/database/entidad_usuaria.dart
+++ lib/src/models/database/entidad_usuaria.dart
@@ -1,5 +1,4 @@
 class DatosUsuarios {
-<<<<<<< HEAD
   int? idUsuario,
    duracionUsual,
     frecuenciaRelacionesMes;
@@ -8,17 +7,4 @@ class DatosUsuarios {
    inicioUltimoPeriodo, finalizoUltimoPeriodo, tomasMuchoLiquido;
 
  
-=======
-  int?  idUsuario,
-        duracionUsual, 
-        frecuenciaRelacionesMes;
-  String? nombre,
-          correo,
-          clave,
-          createdAt,
-          birthday,
-          inicioUltimoPeriodo,
-          finalizoUltimoPeriodo,
-          tomasMuchoLiquido;
->>>>>>> 8b16070061ce2f662cc24302eecb6e5abadc1c16
 }
